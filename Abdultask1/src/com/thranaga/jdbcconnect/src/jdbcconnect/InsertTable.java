9baf5ad9b8b71cd9ce0da71e8bbdc59b084f8b3e
Abdulzabeer <42434400+Abdulzabeer@users.noreply.github.com>
2018-11-24 10:56:06 +0530

    Add files via upload

---
+++ Abdultask1/src/com/thranaga/jdbcconnect/src/jdbcconnect/InsertTable.java
@@ -0,0 +1,52 @@
+package jdbcconnect;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public class InsertTable {
+
+	public static void main(String[] args) {
+		try
+		{
+			Class.forName("org.h2.Driver");
+			
+			Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/thranaga","root","root");
+PreparedStatement psmt=conn.prepareStatement("insert into Employee values(?,?,?,?)");
+			
+			int empid,salary;
+			String empName,addr;
+			
+			Scanner scanner=new Scanner(System.in);
+			
+			System.out.println("Enter the Employee ID");
+			empid=scanner.nextInt();
+			System.out.println("Enter the Employee Name");
+			empName=scanner.next();
+			System.out.println("Enter the Employee Address");
+			addr=scanner.next();
+			System.out.println("Enter the Employee Salary");
+			salary=scanner.nextInt();
+			
+			psmt.setInt(1, empid);
+			psmt.setString(2, empName);
+			psmt.setString(3, addr);
+			psmt.setInt(4, salary);
+			
+			int row_eff=psmt.executeUpdate();
+			
+			if(row_eff>0)
+				System.out.println("Row is Inserted");
+			else
+				System.out.println("Problem Arised:");
+			
+			psmt.close();
+			conn.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception Arised:"+e);
+		}
+	}
+
+		}
