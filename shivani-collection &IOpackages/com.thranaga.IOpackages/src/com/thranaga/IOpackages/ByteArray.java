1217c998be95e053cf5392908c365b79fe05c8c4
Shivanisura <45191922+Shivanisura@users.noreply.github.com>
2018-11-24 17:39:18 +0530

    Add files via upload

---
+++ shivani-collection &IOpackages/com.thranaga.IOpackages/src/com/thranaga/IOpackages/ByteArray.java
@@ -0,0 +1,19 @@
+package com.thranaga.IOpackages;
+import java.io.*;  
+
+public class ByteArray {
+	public static void main(String args[])throws Exception{    
+	      FileOutputStream fout1=new FileOutputStream("D:\\f1.txt");    
+	      FileOutputStream fout2=new FileOutputStream("D:\\f2.txt");    
+	        
+	      ByteArrayOutputStream bout=new ByteArrayOutputStream();    
+	      bout.write(65);    
+	      bout.writeTo(fout1);    
+	      bout.writeTo(fout2);    
+	        
+	      bout.flush();    
+	      bout.close();//has no effect    
+	      System.out.println("Success...");    
+	     }    
+
+}
