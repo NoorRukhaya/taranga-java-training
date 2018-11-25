12c92a1c82f985e65f66e79cc7df6446f0d64103
prema <prema@192.168.0.2>
2018-11-25 11:40:06 +0530

    check

--- Ramya_Task1_JDBC/src/jdbcconnection/CURDDemo2.java
+++ Ramya_Task1_JDBC/src/jdbcconnection/CURDDemo2.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class CURDDemo2 {
-	
+	 
 	public static void main(String[] args) {  
 		try{  
 			Class.forName("com.mysql.cj.jdbc.Driver");
