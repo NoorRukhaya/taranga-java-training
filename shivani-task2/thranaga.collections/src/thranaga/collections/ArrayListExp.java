b71456d89113760bca341a86b8095e61c71d9b42
Shivanisura <45191922+Shivanisura@users.noreply.github.com>
2018-11-27 14:21:40 +0530

    Add files via upload

---
+++ shivani-task2/thranaga.collections/src/thranaga/collections/ArrayListExp.java
@@ -0,0 +1,16 @@
+package thranaga.collections;
+import java.util.*;
+
+public class ArrayListExp {
+	public static void main(String args[]){  
+		ArrayList<String> list=new ArrayList<String>();    
+		      list.add("Ravi");    
+		      list.add("Vijay");    
+		      list.add("Ravi");    
+		      list.add("Ajay");    
+		         
+		      System.out.println(list);  
+		  }  
+		 
+
+}
---
+++ shivani-task2/thranaga.collections/src/thranaga/collections/LinkedListExp.java
@@ -0,0 +1,19 @@
+package thranaga.collections;
+import java.util.*;
+
+public class LinkedListExp {
+	 public static void main(String args[]){  
+		  
+		  LinkedList<String> al=new LinkedList<String>();  
+		  al.add("Ravi");  
+		  al.add("Vijay");  
+		  al.add("Ravi");  
+		  al.add("Ajay");  
+		  
+		  Iterator<String> itr=al.iterator();  
+		  while(itr.hasNext()){  
+		   System.out.println(itr.next());  
+		  }  
+		 }  
+
+}
