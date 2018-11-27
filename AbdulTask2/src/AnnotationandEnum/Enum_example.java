02d5144478a9430428766c13b495322ff3573c2a
Abdulzabeer <42434400+Abdulzabeer@users.noreply.github.com>
2018-11-27 12:15:35 +0530

    Add files via upload

---
+++ AbdulTask2/src/AnnotationandEnum/Enum_example.java
@@ -0,0 +1,16 @@
+package AnnotationandEnum;
+import java.util.Scanner; 
+
+public class Enum_example {
+	enum Color 
+    { 
+        RED, GREEN, BLUE; 
+    } 
+  
+    // Driver method 
+    public static void main(String[] args) 
+    { 
+        Color c1 = Color.RED; 
+        System.out.println(c1); 
+    } 	
+ } 
