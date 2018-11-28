5d85998f1c8fd727f2363a5255e9040c67eea229
Sumukhi M S <45203950+SumukhiMS26@users.noreply.github.com>
2018-11-28 16:36:10 +0530

    Add files via upload

---
+++ Sumukhi_Task1/Another.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+ 
+class Another {
+  public static void main(String[] args) {
+    int result;
+   
+    result = Math.min(10, 20); //calling static method min by writing class name
+ 
+    System.out.println(result);
+    System.out.println(Math.max(100, 200));
+  }
+}
