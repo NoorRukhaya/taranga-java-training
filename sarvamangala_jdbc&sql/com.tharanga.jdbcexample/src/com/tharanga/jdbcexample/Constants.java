8800c68ef019187fe230fb6386927e355feddc62
sarvamangala711 <45422105+sarvamangala711@users.noreply.github.com>
2018-12-20 13:04:05 +0530

    Add files via upload

---
+++ sarvamangala_jdbc&sql/com.tharanga.jdbcexample/src/com/tharanga/jdbcexample/Constants.java
@@ -0,0 +1,11 @@
+package com.tharanga.jdbcexample;
+
+public interface Constants {
+	
+	String DRIVER = "org.hsqldb.jdbcDriver";
+	String URL = "jdbc:hsqldb:hsql://localhost/";
+	String UID = "SA";
+	String PASS = "";
+
+	
+}
