6444e835a6ca17fd738f1f0c2865ad18571e886e
Sumukhi M S <45203950+SumukhiMS26@users.noreply.github.com>
2018-12-27 17:11:32 +0530

    Add files via upload

---
+++ Sumukhi_Task1/sumukhi/Final.java
@@ -0,0 +1,12 @@
+
+public class Final {
+	 final int speedlimit=90;//final variable  
+	 void run(){  
+	  System.out.println("the speed is"+speedlimit );  
+	 }  
+	 public static void main(String args[]){  
+	 Final obj=new  Final();  
+	 obj.run();  
+	 }  
+
+}
